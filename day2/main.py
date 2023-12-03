2ee918118fe5573fad145c27302639557a2fc569
jafetsinke <jafet.sinke@student.hu.nl>
2023-12-03 18:08:12 +0100

    day 2 part 2 done

--- day2/main.py
+++ day2/main.py
@@ -7,6 +7,20 @@ class Game:
         rounds_str = game[1].split(";")
         self.rounds = [CubeSet(round) for round in rounds_str]
 
+    def min_cubeset(self):
+        cube_set = CubeSet("0 red, 0 green, 0 blue")
+
+        for round in self.rounds:
+            if round.red > cube_set.red:
+                cube_set.red = round.red
+            if round.green > cube_set.green:
+                cube_set.green = round.green
+            if round.blue > cube_set.blue:
+                cube_set.blue = round.blue
+
+        return cube_set
+
+
     def __repr__(self):
         return f"Game ID: {self.game_id}, Rounds: {self.rounds}"
 
@@ -28,6 +42,9 @@ class CubeSet:
                 self.green = int(cube[0])
             elif cube[1] == "blue":
                 self.blue = int(cube[0])
+
+    def power(self):
+        return self.red * self.green * self.blue
 
     def __repr__(self):
         return f"Red: {self.red}, Green: {self.green}, Blue: {self.blue}"
@@ -56,8 +73,13 @@ for line in lines:
     if game_possible:
         sum_possible_game_ids += game.game_id
 
-print(sum_possible_game_ids)
+print("Part 1:", sum_possible_game_ids)
 
+# part 2
+sum_powers = 0
+for line in lines:
+    game = Game(line)
+    power = game.min_cubeset().power()
+    sum_powers += power
 
-
-
+print("Part 2:", sum_powers)
