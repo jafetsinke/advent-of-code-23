f4dbaacbe8620d4bcd2e9a018a993622677f1122
jafetsinke <jafet.sinke@student.hu.nl>
2023-12-04 11:46:54 +0100

    day 3 part 2 finally done

--- day3/main.py
+++ day3/main.py
@@ -10,8 +10,8 @@ class Schematic:
             chars = list(line)
             self.schematic.append(chars)
 
-        self.width = len(self.schematic)
-        self.height = len(chars)
+        self.height = len(self.schematic)
+        self.width = len(chars)
 
     def get_coords(self, x, y):
         return self.schematic[y][x]
@@ -19,6 +19,16 @@ class Schematic:
     def check_symbol_adjacent(self, x_coord, y_coord):
         # check if the symbol at x,y is adjacent to a symbol, returns boolean
         found_symbol = False
+        neighbours = self.find_neighbours(x_coord, y_coord)
+        for char in neighbours:
+            if is_symbol(char):
+                found_symbol = True
+                break
+        return found_symbol
+    
+    def find_neighbours(self, x_coord, y_coord) -> str:
+        neighbours = ""
+
         for x in range(x_coord - 1, x_coord + 2):
             for y in range(y_coord - 1, y_coord + 2):
                 if x == x_coord and y == y_coord:
@@ -29,10 +39,54 @@ class Schematic:
                     continue
 
                 char = self.get_coords(x, y)
-                if is_symbol(char):
-                    return True
-            
-        return found_symbol
+                neighbours += char
+        
+        return neighbours
+    
+    def find_adjacent_part_nums(self, x_coord, y_coord) -> list:
+        
+        part_nums = []
+
+        for y in range(y_coord - 1, y_coord + 2):
+            for x in range(x_coord - 1, x_coord + 2):
+                if x == x_coord and y == y_coord:
+                    continue
+                if x < 0 or y < 0:
+                    continue
+                if x >= self.width or y >= self.height:
+                    continue
+
+                char = self.get_coords(x, y)
+                if char.isdigit():
+                    part = self.find_full_part(x, y)
+                    part_nums.append(part)
+
+
+        return list(set(part_nums))
+        
+    def find_full_part(self, x_coord, y_coord) -> str:
+        part_num = self.get_coords(x_coord, y_coord)
+        line = self.schematic[y_coord]
+
+        x = x_coord + 1
+        while x < self.width:
+            char = line[x]
+            if char.isdigit():
+                part_num += char
+            else:
+                break
+            x += 1
+
+        x = x_coord - 1
+        while x >= 0:
+            char = line[x]
+            if char.isdigit():
+                part_num = char + part_num
+            else:
+                break
+            x -= 1
+
+        return part_num
 
 
     def draw(self):
@@ -72,15 +126,24 @@ for y, line in enumerate(schema.schematic):
         
         if (char == ".") or (is_symbol(char)) or (x == schema.width - 1):
             if part_valid:
-                print("Part valid", current_part)
                 parts.append(current_part)
 
             current_part = ""
             part_valid = False
             continue
         
-                
 
-print(parts)
+print("part 1", sum([int(part) for part in parts]))
 
-print(sum([int(part) for part in parts]))
+# Part 2 
+sum_gear_ratio = 0
+for y, line in enumerate(schema.schematic):
+    for x, char in enumerate(line):
+        if is_symbol(char):
+            adjacent_parts = schema.find_adjacent_part_nums(x, y)
+            if (len(adjacent_parts) == 2):
+                gear_ratio = int(adjacent_parts[0]) * int(adjacent_parts[1])
+                sum_gear_ratio += gear_ratio
+
+# print(schema.find_adjacent_part_nums(9, 1))
+print("part 2", sum_gear_ratio)
